18bd30b55295bff07e34f7abc19ac865a04dcfd7
Varana Navadiya <varananavadiya@Varanas-MacBook-Air.local>
2024-09-28 20:37:29 -0400

    Lab_3 module2

--- Lab_3/src/model/AccountDirectory.java
+++ Lab_3/src/model/AccountDirectory.java
@@ -4,10 +4,34 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author varananavadiya
  */
 public class AccountDirectory {
+    private ArrayList<Account> accounts;
     
+    public AccountDirectory(){
+        this.accounts= new ArrayList<Account>();
+    }
+    
+   public ArrayList<Account> getAccounts(){
+       return accounts;
+   }
+    
+   public void setAccounts(ArrayList<Account> accounts){
+        this.accounts=accounts;
+   }
+   
+   public Account addAccount(){
+       Account a=new Account();
+       accounts.add(a);
+       return a;
+   }
+   public void deleteAccount(Account account){
+       accounts.remove(account);
+   
+   }
 }
