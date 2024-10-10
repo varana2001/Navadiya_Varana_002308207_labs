3f6b79a7da633d7d81f8d17cf14dfde8ec2dc98f
VaranaNavadiyaNU <navadiya.v@northeastern.edu>
2024-10-10 13:01:56 -0400

    Push1

---
+++ Lab_4/src/model/Feature.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author varananavadiya
+ */
+public class Feature {
+    private Product owner;
+    private String name;
+    private Object value;
+
+    public Product getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Product owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
+
+
