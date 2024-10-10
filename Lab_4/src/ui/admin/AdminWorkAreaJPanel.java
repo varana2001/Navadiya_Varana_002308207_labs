3dfb33278c6884ae7cce10eba23626193a0aef41
VaranaNavadiyaNU <navadiya.v@northeastern.edu>
2024-10-10 14:17:12 -0400

    Push4-AdminWorkAreaJPanel

--- Lab_4/src/ui/admin/AdminWorkAreaJPanel.java
+++ Lab_4/src/ui/admin/AdminWorkAreaJPanel.java
@@ -103,14 +103,22 @@ public class AdminWorkAreaJPanel extends javax.swing.JPanel {
 
     private void btnManageSuppliersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageSuppliersActionPerformed
         // TODO add your handling code here:
-     
+        ManageSuppliers panel= new ManageSuppliers(workArea,supplierDirectory);
+        workArea.add("ManageSupplierAdministrative",panel);
+        CardLayout layout=(CardLayout) workArea.getLayout();
+        layout.next(workArea);
     }//GEN-LAST:event_btnManageSuppliersActionPerformed
 
     private void btnLogOutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogOutActionPerformed
         // TODO add your handling code here:
+        mainWorkArea.remove(this);
+        Component[] componentArray= mainWorkArea.getComponents();
+        Component component= componentArray[componentArray.length-1];
+        LoginScreen loginPanel= (LoginScreen)component;
+        loginPanel.populateSupplierCombo();
         
- 
-                
+        CardLayout layout=(CardLayout) mainWorkArea.getLayout();
+        layout.previous(mainWorkArea);
     }//GEN-LAST:event_btnLogOutActionPerformed
 
     @Override
