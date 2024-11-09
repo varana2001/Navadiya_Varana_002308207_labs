1b5a18c4c1847f105ba6874a4083144662ab66b6
VaranaNavadiyaNU <navadiya.v@northeastern.edu>
2024-11-09 09:15:40 -0500

    Push3

--- Lab_6/src/model/MasterOrderList.java
+++ Lab_6/src/model/MasterOrderList.java
@@ -4,10 +4,26 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author varananavadiya
  */
 public class MasterOrderList {
+    ArrayList<Order> orderList;
+
+    public MasterOrderList() {
+        orderList= new ArrayList<Order> ();
+    }
+
+    public ArrayList<Order>  getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+    
     
 }
---
+++ Lab_6/src/model/Order.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author varananavadiya
+ */
+public class Order {
+    ArrayList<OrderItem> orderItemList;
+
+    public Order() {
+        this.orderItemList= new  ArrayList<OrderItem> ();
+        
+    }
+}
