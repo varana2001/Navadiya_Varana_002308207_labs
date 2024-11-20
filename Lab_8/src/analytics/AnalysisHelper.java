7b61644786b440aed596f4958fe8e4e09fd32471
VaranaNavadiyaNU <navadiya.v@northeastern.edu>
2024-11-20 00:00:54 -0500

    Add q6-7

--- Lab_8/src/analytics/AnalysisHelper.java
+++ Lab_8/src/analytics/AnalysisHelper.java
@@ -123,4 +123,47 @@ public class AnalysisHelper {
             System.out.println(users.get(i) + ", - Comment count: " + commentNumbers.get(users.get(i).getId()));
         }
     }
+
+    public void getPassiveAndActiveOverallUsers() {
+
+        DataStore data = DataStore.getInstance();
+
+        HashMap<Integer, Integer> overallNumbers = new HashMap<Integer, Integer>();
+
+        for (Comment c : data.getComments().values()) {
+
+            int userId = c.getUserId();
+            if (overallNumbers.containsKey(userId)) {
+                overallNumbers.put(userId, overallNumbers.get(userId) + 1);
+            } else {
+                overallNumbers.put(userId, 1);
+            }
+        }
+
+        for (Post p : data.getPosts().values()) {
+
+            int userId = p.getUserId();
+            if (overallNumbers.containsKey(userId)) {
+                overallNumbers.put(userId, overallNumbers.get(userId) + 1);
+            } else {
+                overallNumbers.put(userId, 1);
+            }
+        }
+
+        ArrayList<User> users = new ArrayList(data.getUsers().values());
+
+        Collections.sort(users, new UserMapComparator(overallNumbers));
+
+        System.out.println("Q6 The following users have overall been passive : ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Overall Count : " + overallNumbers.get(users.get(i).getId()));
+        }
+
+        System.out.println("Q7 The following users have overall been active : ");
+
+        for (int i = users.size() - 1; i > users.size() - 6; i--) {
+            System.out.println(users.get(i) + ", - Overall count : " + overallNumbers.get(users.get(i).getId()));
+        }
+
+    }
 }
